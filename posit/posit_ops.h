e209be6ab9d4ede79e0f241a56319fb5d69ec114
Edwin Peguero <emp238@cornell.edu>
2019-11-27 15:18:03 -0500

    added subtraction, multiplication, and division operations on posits. See posit_test.c for examples.

--- posit/posit_ops.h
+++ posit/posit_ops.h
@@ -1,3 +1,6 @@
 #include "posit.h"
 
 posit32_t p32_add( posit32_t a, posit32_t b );
+posit32_t p32_sub( posit32_t a, posit32_t b );
+posit32_t p32_mul( posit32_t a, posit32_t b );
+posit32_t p32_div( posit32_t a, posit32_t b );
--- posit/posit_test.c
+++ posit/posit_test.c
@@ -16,9 +16,24 @@ int main() {
   printf("\nposit representation of %f: ", float_val);
   print_posit32_binary(p);
   
-  float x = -1, y = 100;
+  float x, y, z;
+  x = -1, y = 100;
   printf("Posit add test:\n");
-  float z = convertP32ToFloat(p32_add(convertFloatToP32(x), convertFloatToP32(y)));
+  z = convertP32ToFloat(p32_add(convertFloatToP32(x), convertFloatToP32(y)));
   printf("%f + %f = %f\n", x, y, z);
-}
 
+  x = 100, y = -100;
+  printf("Posit sub test:\n");
+  z = convertP32ToFloat(p32_sub(convertFloatToP32(x), convertFloatToP32(y)));
+  printf("%f - %f = %f\n", x, y, z);
+
+  x = 100, y = -50;
+  printf("Posit mul test:\n");
+  z = convertP32ToFloat(p32_mul(convertFloatToP32(x), convertFloatToP32(y)));
+  printf("%f * %f = %f\n", x, y, z);
+
+  x = 100, y = -4;
+  printf("Posit div test:\n");
+  z = convertP32ToFloat(p32_div(convertFloatToP32(x), convertFloatToP32(y)));
+  printf("%f / %f = %f\n", x, y, z);
+}
